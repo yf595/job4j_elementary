b4fe5e0604cf845dbdcb6f8105cd3c0ff7f826fb
yf595 <yf595@nyu.edu>
2022-06-19 22:00:36 +0300

    4.1. Разбор первой программы. [#159033 #314202]

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -2,13 +2,13 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        System.out.println("1 * 2 = 2\n" +
-                "1 * 3 = 3\n" +
-                "1 * 4 = 4\n" +
-                "1 * 5 = 5\n" +
-                "1 * 6 = 6\n" +
-                "1 * 7 = 7\n" +
-                "1 * 8 = 8\n" +
-                "1 * 9 = 9\n");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
     }
 }
