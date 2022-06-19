bf05634171661463f6999d02e2af9c63c7fe789a
yf595 <yf595@nyu.edu>
2022-06-19 20:24:13 +0300

    6. Checkstyle [#4123]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,14 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1 * 2 = 2\n" +
+                "1 * 3 = 3\n" +
+                "1 * 4 = 4\n" +
+                "1 * 5 = 5\n" +
+                "1 * 6 = 6\n" +
+                "1 * 7 = 7\n" +
+                "1 * 8 = 8\n" +
+                "1 * 9 = 9\n");
+    }
+}
