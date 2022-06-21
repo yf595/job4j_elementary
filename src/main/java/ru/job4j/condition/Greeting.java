2b681dd1b22daebd66f21c3862adfeab6bd76d01
yf595 <yf595@nyu.edu>
2022-06-21 14:14:32 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+
+        String idea = " I like Java!";
+
+        System.out.println(idea);
+
+        idea += " But I am a newbie "; // - присоедините к переменной idea строку "But I am a newbie.";
+
+        int year = 2022; // - создайте переменную year с типом int и присвойте ей текущий год.
+
+        idea += year; // - присоедините к переменной idea переменную year;
+
+        System.out.println(idea); //;- выведите на консоль эту переменную через команду System.out.println(idea);
+
+    }
+}
